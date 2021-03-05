b36fdd4da5ae3c4670924d65706aeebeec6ab7dd
marcosgomesbr <marcoseduardo01@icloud.com>
2021-03-04 22:01:22 -0300

    EMF5 Finalizado

--- Estudos/src/MudancaFluxo/EMF5.java
+++ Estudos/src/MudancaFluxo/EMF5.java
@@ -1,9 +1,27 @@
 package MudancaFluxo;
 
+import java.util.Scanner;
+
 public class EMF5 {
 
 	public static void main(String[] args) {
-
+		Scanner l = new Scanner(System.in);
+		double nota1 = 0;
+		double nota2 = 0;
+		double media = 0;
+		System.out.println("---MEDIA ALUNO---");
+		System.out.println("Nota 1: ");
+		nota1 = l.nextDouble();
+		System.out.println("Nota 2: ");
+		nota2 = l.nextDouble();
+		media = nota1 + nota2 / 2;
+		if (media >=  7 && media <= 9) {
+			System.out.println("ALUNO APROVADO");
+		} else if (media >= 10) {
+			System.out.println("APROVADO DISTINCAO");
+		} else {
+			System.out.println("REPROVADO");
+		}
 	}
 
 }
---
+++ Estudos/src/MudancaFluxo/EMF6.java
@@ -0,0 +1,15 @@
+package MudancaFluxo;
+
+public class EMF6 {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 9;
+		int num3 = 7;
+		if (num1 > num2 && num1 > num3 ) {
+			System.out.println(num1);
+		}
+		//PAREI AQUI
+	}
+
+}
